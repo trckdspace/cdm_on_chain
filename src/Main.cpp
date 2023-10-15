cff76dff14e3084ac4d02439e7459038844ab827
ylatif <yasir.latif@gmail.com>
2023-10-15 22:39:44 +1030

    using stored data if not able to get from space-track. A failsafe

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,11 +11,14 @@
 
 int main(int argc, char *argv[])
 {
-
+	std::string data_source = "./3le.txt";
 	if (!SpaceTrackLiveData::get_latest_tles("./3le.txt"))
-		return -1;
+	{
+		std::cerr << "Using stored data instead " << std::endl;
+		data_source = "../data/3le.txt";
+	}
 
-	CBlockChain bChain = CBlockChain("./3le.txt", 2);
+	CBlockChain bChain = CBlockChain(data_source.c_str(), 2);
 
 	bChain.SetPropagationResolutionSeconds(0.01);
 
